58a7f225da14a459664d38d68e94a79b6986364d
Elena <el.zaharieva28@gmail.com>
2015-04-16 22:16:49 +0300

    added spaln map_print

--- Dungeon.py
+++ Dungeon.py
@@ -1,7 +1,17 @@
+from Hero import Hero
+
+
+class NotAHero(Exception):
+    pass
+
+
 class Dungeon:
 
-    def __init__(self):
+    def __init__(self, hero):
         self.list = []
+        self.hero_position_x = -1
+        self.hero_position_y = -1
+        self.hero = hero
 
     def map_reading(self, path):
         text_file = open(path, "r")
@@ -11,5 +21,21 @@ class Dungeon:
         self.list = [list(l) for l in self.list]
         return self.list
 
+    def print_map(self):
+        for l in self.list:
+            print("".join(l))
+
+    def spawn(self, hero):
+        if not (isinstance(hero, Hero)):
+            raise NotAHero
+        for l in self.list:
+            for x in range(0, len(l)):
+                if l[x] == 'S':
+                    l[x] = 'H'
+                    return True
+
 a = Dungeon()
-print(a.map_reading('game_map.txt'))
+a.map_reading('game_map.txt')
+my_hero = Hero("Bron", "Dragonslayer", 100, 100, 2)
+print(a.spawn(my_hero))
+a.print_map()
