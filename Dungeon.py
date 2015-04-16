fb768182d5b6b29b8eea94347594b30d310e605c
Hrizantema  Stancheva <hrizantema.st@gmail.com>
2015-04-16 19:18:34 +0300

    Move function added

--- Dungeon.py
+++ Dungeon.py
@@ -1,7 +1,10 @@
 class Dungeon:
 
-    def __init__(self):
+    def __init__(self, hero):
         self.list = []
+        self.hero_position_x = -1
+        self.hero_position_y = -1
+        self.hero = hero
 
     def map_reading(self, path):
         text_file = open(path, "r")
@@ -11,5 +14,44 @@ class Dungeon:
         self.list = [list(l) for l in self.list]
         return self.list
 
+    def move_hero(self, direction):
+    if direction == "up":
+        if self.hero_position_y == 0:
+            return False
+        next_x_position = self.hero_position_x
+        next_y_position = self.hero_position_y - 1
+
+    if direction == "down":
+        if self.hero_position_y == len(self.list) - 1:
+            return False
+        next_x_position = self.hero_position_x
+        next_y_position = self.hero_position_y + 1
+
+    if direction == "left":
+        if self.hero_position_x == 0:
+            return False
+        next_y_position = self.hero_position_y
+        next_x_position = self.hero_position_x - 1
+
+    if direction == "right":
+        if self.hero_position_x == len(self.list[1]) - 1:
+            return False
+        next_x_position = self.hero_position_x + 1
+        next_y_position = self.hero_position_y
+
+        if self.list[next_x_position][next_y_position] == "#":
+            return False
+        if self.list[next_x_position][next_y_position] == "E":
+            pass # start a fight
+        if self.list[next_x_position][next_y_position] == "T":
+            pass
+        if self.list[next_x_position][next_y_position] == ".":
+            self.hero_position_x = next_x_position
+            self.hero_position_y = next_y_position
+            return True
+
+
 a = Dungeon()
 print(a.map_reading('game_map.txt'))
+
+
